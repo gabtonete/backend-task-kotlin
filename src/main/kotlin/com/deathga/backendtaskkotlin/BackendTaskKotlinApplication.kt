05f1f6106b595d15d2fabef9a87d6d86619aed09
Gabriel Tonete <gabtopi77@gmail.com>
2022-03-18 11:41:13 -0300

    [Creation] Kotlin API REST creation with Spring boot

---
+++ src/main/kotlin/com/deathga/backendtaskkotlin/BackendTaskKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.deathga.backendtaskkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendTaskKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendTaskKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/deathga/backendtaskkotlin/BackendTaskKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.deathga.backendtaskkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendTaskKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
